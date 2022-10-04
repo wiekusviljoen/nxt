9cd0105ffeff5f7f2c2909ea3997ccbc9fc74f00
wiekusviljoen <wiekusviljoen@gmail.com>
2022-10-04 20:21:43 +0200

    commit on master

---
+++ pages/news/[newsId].js
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+function DetailPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  return <h1>News Details</h1>;
+}
+
+export default DetailPage;
