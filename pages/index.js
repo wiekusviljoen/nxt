cc0f591ee33aa07ccc2206ce00ae8238052a2d70
wiekusviljoen <wiekusviljoen@gmail.com>
2022-10-08 20:17:54 +0200

    commit on master

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-function HomePage() {
-  return <h1>Home</h1>;
-}
+import Head from "next/head";
+import { Toolbar } from "../components/toolbar";
+import styles from "../styles/Home.module.css";
 
-export default HomePage;
+export default function Home() {
+  return (
+    <div className="page-container">
+      <Toolbar />
+      <div className={styles.main}>
+        <h1>News</h1>
+        <h3>Your one stop shop for the latest news</h3>
+      </div>
+    </div>
+  );
+}
